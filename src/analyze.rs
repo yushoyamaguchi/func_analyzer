e7f5c11508cbe3392ff147ae4d1d74212a899b7c
yushoyamaguchi <ysh.824@outlook.jp>
2024-01-27 19:20:12 +0900

    read file and put in vec

--- src/analyze.rs
+++ src/analyze.rs
@@ -18,10 +18,14 @@ impl FunctionNode {
     }
 }
 
+// ある関数が何行目にあるかのハッシュテーブル
+
+
 // nameの関数の中身を調べて、そこから呼び出してる関数を子として登録する
 // 子のノードに対しては,nameとcurr_depthだけ設定する
 // 他のノードとの子の重複はとりあえずチェックしない
-fn parse_c_fn(source: &str, fn_node: &Arc<Mutex<FunctionNode>>) {
+// ある関数の定義が何行目にあるかのハッシュテーブルを参照する
+fn parse_c_fn(source: &Vec<&str>, fn_node: &Arc<Mutex<FunctionNode>>) {
     // C言語の関数をパースするロジックをここに実装
     print!("parse_c_fn() is not implemented yet");
 }
@@ -30,7 +34,7 @@ fn parse_c_fn(source: &str, fn_node: &Arc<Mutex<FunctionNode>>) {
 // まずは深さチェック
 // 次に自分の関数があるかチェックして、あれば自分が呼び出してる関数を子として全て格納
 // 全ての子に対して再帰的にこの関数を呼び出す
-fn search_c_fn(source: &str, depth:i64, fn_node: &Arc<Mutex<FunctionNode>>) {
+fn search_c_fn(source: &Vec<&str>, depth:i64, fn_node: &Arc<Mutex<FunctionNode>>) {
     let fn_node_locked = fn_node.lock().unwrap();
     if depth == fn_node_locked.curr_depth {
         return;
@@ -45,11 +49,10 @@ fn search_c_fn(source: &str, depth:i64, fn_node: &Arc<Mutex<FunctionNode>>) {
 
 
 // Call Graphを生成するための関数
-pub fn generate_call_graph(source: &str, depth: i64, root: Arc<Mutex<FunctionNode>>)  {
+pub fn generate_call_graph(source: &Vec<&str>, depth: i64, root: Arc<Mutex<FunctionNode>>)  {
     let src_c_file = source;
     let depth = depth;
-
-    let root_fn_name = root.lock().unwrap().name.clone();
+    search_c_fn(source, depth, &root);
     
 }
 
--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,40 @@ use std::sync::{Arc, Mutex};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    print!("args: {:?}", args);
     if args.len() != 2 {
         eprintln!("Usage: {} <config_file>", args[0]);
         return;
     }
 
+    // configファイルを読み込んで処理する
     let config_file = &args[1];
     let file = fs::File::open(config_file).expect("Failed to open config file");
     let mut lines = io::BufReader::new(file).lines();
-
     let c_source_file = lines.next().expect("Expected C source file path").expect("Failed to read line");
     let target_function = lines.next().expect("Expected target function name").expect("Failed to read line");
     let depth_str = lines.next().expect("Expected depth").expect("Failed to read line");
     let depth: i64 = depth_str.parse().expect("Depth must be a number");
 
-    let source_code = fs::read_to_string(&c_source_file)
-        .expect("Failed to read C source file");
 
-        let root = Arc::new(Mutex::new(FunctionNode::new(target_function)));
-        let root_clone = Arc::clone(&root);
-        generate_call_graph(&source_code, depth, root_clone);
-        let root_clone2 = Arc::clone(&root);
-        output_yaml(root_clone2);
+    // C言語のソースファイルを読み込んで処理する
+    let c_file = fs::File::open(c_source_file).expect("Failed to open C source file");
+    let reader = io::BufReader::new(c_file);
+    let mut temp_lines: Vec<String> = Vec::new(); // 一時的なStringのVec
+    for line in reader.lines() {
+        match line {
+            Ok(ln) => temp_lines.push(ln),
+            Err(e) => println!("Error reading line: {}", e),
+        }
+    }
+    let  c_src_lines: Vec<&str> = temp_lines.iter().map(AsRef::as_ref).collect();
+
+
+    // Call Graphを生成するための処理を呼び出す
+    let root = Arc::new(Mutex::new(FunctionNode::new(target_function)));
+    let root_clone = Arc::clone(&root);
+    generate_call_graph(&c_src_lines, depth, root_clone);
+    let root_clone2 = Arc::clone(&root);
+    output_yaml(root_clone2);
 }
 
