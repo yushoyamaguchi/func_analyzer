7787f7b851d75e61699c1d171cac37284f687ed2
yushoyamaguchi <ysh.824@outlook.jp>
2024-01-30 20:36:29 +0900

    ref

--- src/analyze.rs
+++ src/analyze.rs
@@ -163,12 +163,14 @@ impl Parser {
     // 次に自分の関数があるかチェックして、あれば自分が呼び出してる関数を子として全て格納
     // 全ての子に対して再帰的にこの関数を呼び出す
     fn search_c_fn(&mut self, depth:usize, fn_node: &Rc<RefCell<FunctionNode>>) {
-        let fn_node_locked = fn_node.borrow_mut();
-        println!("serch_c_fn: {} depth: {}", fn_node_locked.name, fn_node_locked.curr_depth);
-        if depth == fn_node_locked.curr_depth {
-            return;
+        {
+            let fn_node_locked = fn_node.borrow_mut();
+            println!("fn_name: {}, curr_depth: {}", fn_node_locked.name, fn_node_locked.curr_depth);
+            if depth == fn_node_locked.curr_depth {
+                return;
+            }
         }
-        self.parse_c_fn(fn_node);
+        self.parse_c_fn(&Rc::clone(fn_node));
         // 子に対して再帰的にこの関数を呼び出す
         let fn_node_locked = fn_node.borrow_mut();
         for child in fn_node_locked.calls.iter() {
@@ -180,9 +182,7 @@ impl Parser {
     pub fn generate_call_graph(&mut self, depth: usize)  {
         let depth = depth;
         let root_clone = Rc::clone(&self.root);
-        // cloneでいいのかチェック
         self.search_c_fn(depth, &root_clone);
-        
     }
 
 }
